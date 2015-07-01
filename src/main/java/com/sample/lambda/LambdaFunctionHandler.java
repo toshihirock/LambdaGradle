f70da011c3bd3631775b573dd0d36c4086b7a631
toshihirock <toshihiro308@gmail.com>
2015-07-02 06:41:47 +0900

    add log

--- src/main/java/com/sample/lambda/LambdaFunctionHandler.java
+++ src/main/java/com/sample/lambda/LambdaFunctionHandler.java
@@ -2,14 +2,15 @@ package com.sample.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-
 import com.amazonaws.services.lambda.runtime.events.S3Event;
 
 public class LambdaFunctionHandler implements RequestHandler<S3Event, Object> {
 
     @Override
     public Object handleRequest(S3Event input, Context context) {
+    	context.getLogger().log("start");
         context.getLogger().log("Input: " + input);
+        context.getLogger().log("end");
 
         // TODO: implement your handler
         return null;
